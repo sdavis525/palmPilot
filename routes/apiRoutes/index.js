e9f3abb7520dfeac7277d49d423b6ac9f9c5ec02
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-04-26 16:34:48 -0400

    working on a different route, added new folders/ updated apiRoutes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { createNewNote } = require('../../lib/newNote');
+const { newNotes } = require('../../develop/db/db.json');
+const { uniUUID } = require('uuid')
+
+
+router.get('newNotes', (req, res) => {
+  let results = newNotes ;
+  if (results) {
+    res.json(results);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post('/newNotes', (req, res) => {
+  req.body.id = uniUUID();
+    const notes = createNewNote(req.body, newNotes);
+    res.json(notes);
+  
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,47 +1,17 @@
 const express = require('express');
-const path = require("path");
-const fs = require('fs');
-const textNote = require("./develop/db/db.json");
-const { uniUUID }  = require('code');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-
-
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(textNote)
-});
-
-
-app.post('/api/notes', (req, res) => {
-    const newEntry = req.body;
-    newEntry.id = uniUUID();
-    db.push(newEntry);
-    fs.writeFileSync(
-        path.join(__dirname, './develop/db/db.json'),
-        JSON.stringify(db)
-    );
-    res.json(newEntry);
-});
-
-
-//Create a Route for homepage
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// Create a Route for output page
-app.get('/output', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
