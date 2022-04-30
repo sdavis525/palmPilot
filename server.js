71a2033764848091a380479c379c88ac7caedaf6
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-04-30 10:38:59 -0400

    changed port code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
